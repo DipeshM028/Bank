38b596a6ce823a2edf1e47cc7e4d0395769f1e09
DipeshM028 <78720547+DipeshM028@users.noreply.github.com>
2021-09-20 12:10:45 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Basic Banking System</title>
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/style.css">
+    <!-- <link rel="stylesheet" href="css/navbar.css"> -->
+</head>
+
+<body>
+     <header class="head">
+           <div class="left">
+              <img src="images/logo.png" style="height: 70px; width: 150px;">
+           
+           <div class="topnav">
+               <ul class="navbar">
+                   <li class="navlink"><a href="index.php">Home</a>
+                 </li>
+                   <li class="navlink"><a href="view user.php">View Customer</a>
+                </li>
+                   <li class="navlink"><a href="transfermoney.php">Transfer-money</a>
+                </li>
+              <li class="navlink"><a href="transfer history.php">Transfer-history</a>
+                </li>
+               </ul>
+           </div>
+        </div>
+       </header>
+       
+    
+          <div class="container-fluid">
+        <!--Intro page-->
+        <div class="intro row py1">
+            <div class="intro col-md">
+                <div class="text-center my-6">
+       <h3>Welcome To</h3>
+       <h1>Spark Bank</h1>
+    </div>
+
+</div>
+      <div>
+        <img src="images/download.jpg" class="img" style="height: 320px; width: 500px;">
+   </div>
+    </div>
+    </div>
+    <!--Activity section -->
+    <div class="row">
+       <div class="col">
+           <img src="images/user.jpg" class="img-fluid">
+           <br>
+        
+           <a class="bb" href="view user.html">
+               <button>View Customer</button>
+            </a>
+       </div>
+
+       <div class="col">
+           <img src="images/tracestion.jpg" class="img-fluid">
+           <br>
+           <a class="bb" href="transfermoney.html">
+               <button> Make a Transection</button>
+            </a>
+       </div>
+
+           <div class="col">
+               <img src="images/hostory.jpg" class="img-fluid">
+               <br>
+               <a class="bb" href="transfer history.php">
+                   <button>Transfer History</button>
+                </a>
+           </div>
+           </div>
+<div class="feet">
+           <footer class="foot">
+               <p>#GRIPintern2021 Made by:<b>Dipesh Malviya</b><br>The Spark Foundation</p>
+           </footer>
+        </div>
+    </div>
+    </div>
+</body>
+</html>
